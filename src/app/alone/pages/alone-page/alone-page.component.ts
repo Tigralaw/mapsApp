01262af8f4293e327a7684f926479c5d072eff54
Laura Alarcon <laura.alarcon@durango.gob.mx>
2024-04-11 12:10:20 -0600

    Standalone como componente reutilizable

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 // Standalone component básico
 @Component({
   standalone: true,
   // se importa CommonModule para poder utilizar las directivas de angular en el html coo *ngIf, *ngFor, etc...
   // imports: [ CommonModule ],
+  imports: [ CounterAloneComponent ],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
